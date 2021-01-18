f8c3ba66c7d2a232e42b5a7db05d0240200abc46
travisabinadismith <49415063+travisabinadismith@users.noreply.github.com>
2021-01-18 16:55:55 -0700

    Implemented SortTasks component and ToDoListMenu component

--- todo-list-frontend/src/resources/components/ToDoListMenu.js
+++ todo-list-frontend/src/resources/components/ToDoListMenu.js
@@ -1,4 +1,54 @@
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import SortTasks from './SortTasks'
+import { removeToDoItem, addToDoItem, updateCurrItem } from '../store/actionCreators'
+import React from 'react'
+import { connect } from 'react-redux'
+import { ReactReduxContext } from 'react-redux'
+import Tooltip from '@material-ui/core/Tooltip';
 
-const ToDoListMenu = () => <div className="toDoListMenu"></div>
+const ToDoListMenuOptions = ({ addItem, updateCurrToDoItem }) => <div className="toDoListMenu">
+        <ReactReduxContext.Consumer>
+            {({ store }) => {
+                return <div>
+                    <SortTasks currFilter={store.getState().currState.filter} />
+                    <Tooltip title="Add">
+                        <IconButton edge="end" aria-label="edit" className="addItemBtn" onClick={() => {
+                            addItem();
+                            //Get the most recent item from the list of items
+                            var newItem = store.getState().toDoItems[store.getState().toDoItems.length - 1]
+                            //Update the newest item
+                            updateCurrToDoItem(newItem);
+                        }} >
+                            <AddIcon />
+                        </IconButton>
+                    </Tooltip>
 
-export default ToDoListMenu
+                </div>
+            }
+            }
+        </ReactReduxContext.Consumer>
+    </div>
+
+const mapStateToProps = (state) => {
+    return state;
+}
+
+const mapDispatchToProps = dispatch =>
+({
+    addItem() {
+        dispatch(addToDoItem())
+    },
+    removeItem(id) {
+        dispatch(removeToDoItem(id))
+    },
+    updateCurrToDoItem(item) {
+        dispatch(updateCurrItem(item))
+    }
+})
+const ToDoListMenu = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ToDoListMenuOptions)
+
+export default ToDoListMenu;
