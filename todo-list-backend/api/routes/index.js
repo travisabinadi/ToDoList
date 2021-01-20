1b67ecd7ef0c4ace2f07dffda8d1b5e70b53e2df
travisabinadismith <49415063+travisabinadismith@users.noreply.github.com>
2021-01-20 00:17:30 -0700

    Implemented node backend

--- todo-list-backend/api/routes/index.js
+++ todo-list-backend/api/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log("You are requesting info")
+  let rawdata = fs.readFileSync('ToDoList.json');
+  var store = JSON.parse(rawdata);
+  console.log(store)
+  res.send(store);
 });
+
+router.post("/", (req, res, next) => {
+  var store = JSON.stringify(req.body);
+  console.log(store);
+  fs.writeFile(path.join(__dirname, '../../api/ToDoList.json'),
+    store,
+    error => (error) ?
+      console.log("Error saving state!", error) :
+      null
+  )
+  return res.send("You Successfully Posted the Store!")
+})
 
 module.exports = router;
