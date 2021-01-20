51851107e0c7b1b9d226278810c473b1307b187f
travisabinadismith <49415063+travisabinadismith@users.noreply.github.com>
2021-01-20 12:46:32 -0700

    File restructuring

--- todo-list-backend/api/client/src/resources/components/App.js
+++
@@ -1,9 +0,0 @@
-import ToDoList from "./ToDoList.js";
-import {Container} from '@material-ui/core'
-
-const App = () =>
-  <Container maxWidth="sm" className="app">
-    <ToDoList className ="todolist"/>
-  </Container>
-
-export default App;
