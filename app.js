79f64c9f69d8d2cc4c9bad8aee1c6dac1c559cf9
travisabinadismith <49415063+travisabinadismith@users.noreply.github.com>
2021-01-20 13:57:13 -0700

    Edit app.js backend

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json()); 
 
 app.use('/', indexRouter);
@@ -45,15 +45,13 @@ app.use(function(req, res, next) {
   //   }
   // }
   // app.use(cors(corsOptions))
-
-if (process.env.NODE_ENV === 'production') {
+  
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '/client', 'index.html'));
   });
-}
 
 
 
