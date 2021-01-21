2fa32be6c66c3bc176f245878d70de658d07421a
travisabinadismith <49415063+travisabinadismith@users.noreply.github.com>
2021-01-20 17:29:30 -0700

    Updated backend for deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 app.use(bodyParser.json()); 
 
 app.use('/', indexRouter);
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client', 'index.html'));
   });
 
 
